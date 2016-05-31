f0ec0055fa74f79584eaac95a034bbd32cac160d
Giulia <giulia.pennati94@gmail.com>
2016-05-31 10:09:00 +0200

    added css and first script

---
+++ script/primaryScript.js
@@ -0,0 +1,14 @@
+ $(function primary() {
+
+    // load the header
+    $(".header").load( "header.html");
+
+	// default behaviour: load homepage
+	$(".main").load( "home.html");
+	
+	// load footer
+	$(".footer").load("footer.html"); 
+
+
+             
+	});
